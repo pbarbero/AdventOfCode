68835c068315d93aea0141b51ef6c076745570ff
pbarbero <pbarberoiriarte@gmail.com>
2017-12-07 20:24:42 +0100

    Day 1: Part two

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -12,7 +12,8 @@ namespace tennis
             Console.Write("Gimme the number: ");
             string strNumber = Console.ReadLine();
 
-            int result = CalculateTheStuff(strNumber);            
+            //int result = CalculateTheStuff(strNumber);            
+            int result = CalculateTheStuff2(strNumber);
 
             Console.WriteLine(string.Format("Result is: {0}", result.ToString()));
         }
@@ -34,6 +35,31 @@ namespace tennis
             if(digits[length - 1] == digits[0])
             {
                 result += (int)Char.GetNumericValue(digits[0]);
+            }
+
+            return result;
+        }
+
+        static int CalculateTheStuff2(string strNumber)
+        {
+            int result = 0;
+            char[] digits = strNumber.ToCharArray();
+            int length = digits.Length;
+            int stepsForward = length / 2;
+
+            for(int i = 0; i < length; i++)
+            {       
+                int nextIt = i + stepsForward;
+
+                if(nextIt >= length)
+                {
+                    nextIt = nextIt - length;
+                }
+
+                if(digits[i] == digits[nextIt])
+                {
+                    result += (int)Char.GetNumericValue(digits[i]);
+                }
             }
 
             return result;
