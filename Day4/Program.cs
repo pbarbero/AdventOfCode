42645b91769a587ab4b5518d0879f7753e70958b
pbarbero <pbarberoiriarte@gmail.com>
2017-12-10 23:50:07 +0100

    Day 4: Part one

---
+++ Day4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string[] lines = System.IO.File.ReadAllLines(@currentDirectory + "/data");            
+            Phrase phrase;
+            int counter = 0;
+            foreach(var line in lines)
+            {
+                phrase = new Phrase(line);
+                if(phrase.IsValid()) counter++;
+            }
+            Console.WriteLine(counter);
+        }
+    }
+
+    class Phrase
+    {
+        public Phrase(){}
+        public Phrase(string line)
+        {
+            this.Words = line.Split(" ").ToList();
+        }
+        public List<string> Words { get; set; }
+
+        public bool IsValid()
+        {
+            for(int i = 0; i < this.Words.Count(); i++)
+            {
+                for(int j = i+1; j < this.Words.Count(); j++)
+                {
+                    if(this.Words[i] == this.Words[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+    }
+}
