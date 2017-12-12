1e11a99899ce2caf5ed4559188318888cf52c24a
pbarbero <pbarberoiriarte@gmail.com>
2017-12-12 23:36:51 +0100

    Day 12: part one

---
+++ Day12/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = System.IO.File.ReadAllLines(@Directory.GetCurrentDirectory() + "/data");
+            int programs = 1;
+
+            Dictionary<int, List<int>> data = new Dictionary<int, List<int>>();
+            char[] delimiters = {' ', ',', '<', '-', '>'};
+            foreach(var line in lines)
+            {
+                List<string> splittedLine = line.Split(delimiters).ToList();
+                splittedLine.RemoveAll(x => string.IsNullOrEmpty(x) || string.IsNullOrWhiteSpace(x));
+                int key = Int32.Parse(splittedLine[0]);
+                splittedLine.RemoveAt(0);
+                List<int> value = new List<int>();
+                splittedLine.ForEach(x => value.Add(Int32.Parse(x)));
+                data.Add(key, value);
+            }
+
+            List<int> alreadyUsed = new List<int>(){0};
+            foreach(int value in data[0])
+            {
+                programs++;
+                alreadyUsed.Add(value);
+                GetProgramsRelated(value, ref programs, ref alreadyUsed, data);
+            }
+            
+            Console.WriteLine(programs);
+        }
+
+        static void GetProgramsRelated(int key, ref int programs, ref List<int> alreadyUsed, Dictionary<int, List<int>> data)
+        {
+            foreach(int value in data[key])
+            {
+                if(!alreadyUsed.Contains(value))
+                
+                {
+                    programs++;
+                    alreadyUsed.Add(value);
+                    GetProgramsRelated(value, ref programs, ref alreadyUsed, data);         
+                }       
+            }
+        }
+    }
+}
