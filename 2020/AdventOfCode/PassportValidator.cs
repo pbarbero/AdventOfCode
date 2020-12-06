60f297b76353e3fd921987a3e809dabdfdb4fdc6
Pilar Barbero <pbarberoiriarte@gmail.com>
2020-12-06 22:02:28 +0100

    Day 4 - part 2

--- 2020/AdventOfCode/PassportValidator.cs
+++ 2020/AdventOfCode/PassportValidator.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace AdventOfCode
 {
@@ -9,7 +10,8 @@ namespace AdventOfCode
     {
         public static int GetNumberOfValids(IEnumerable<string> lines)
         {
-            return GetPassports(lines).Count(x => x.IsValid());
+            var passports =  GetPassports(lines);
+            return passports.Count(x => x.IsValid());
         }
 
         private static IEnumerable<Passport> GetPassports(IEnumerable<string> lines)
@@ -39,9 +41,9 @@ namespace AdventOfCode
             {
                 foreach(var att in p.GetCustomAttributes(true))
                 {
-                    var ra = att as RequiredAttribute;
+                    var ra = (ValidationAttribute)att;
 
-                    if((RequiredAttribute)att != null)
+                    if(ra != null)
                         if(!ra.IsValid(p.GetValue(self, null)))
                             return false;
                 }
@@ -63,23 +65,81 @@ namespace AdventOfCode
             if(splitProperty.Length != 2)
                 throw new Exception($"Property {property} is bad");
 
-            this.GetType().GetProperty(splitProperty[0]).SetValue(this, splitProperty[1]);
+            var propertyType = this.GetType().GetProperty(splitProperty[0]);
+            int value = 0;
+
+            if(int.TryParse(splitProperty[1], out value) && propertyType.PropertyType == typeof(int))
+                propertyType.SetValue(this, value);
+            else
+                propertyType.SetValue(this, splitProperty[1]);
         }
 
         [Required]
-        public string byr { get; set; }
+        [RegularExpression(@"^(\d{4})$")]
+        [Range(1920, 2002)]
+        public int byr { get; set; }
         [Required]
-        public string iyr { get; set; }
+        [RegularExpression(@"^(\d{4})$")]
+        [Range(2010, 2020)]
+        public int iyr { get; set; }
         [Required]
-        public string eyr { get; set; }
+        [RegularExpression(@"^(\d{4})$")]
+        [Range(2020, 2030)]
+        public int eyr { get; set; }
         [Required]
+        [HeightValidation]
         public string hgt { get; set; }
         [Required]
+        [RegularExpression(@"^#[0-9a-f]{6}$")]
         public string hcl { get; set; }
         [Required]
+        [EyeColorValidation]
         public string ecl { get; set; }
         [Required]
+        [RegularExpression(@"^(\d{9})$")]
         public string pid { get; set; }
         public string cid { get; set; }
+    }
+
+    class HeightValidation: ValidationAttribute
+    {
+        private const string RegexCm  = @"(\d+)cm";
+        private const string RegexIn  = @"(\d+)in";
+
+        public override bool IsValid(object value)
+        {
+            var strvalue = (string)value;
+            var regexCm = new Regex(RegexCm);
+            var regexIn = new Regex(RegexIn);
+
+            if(regexCm.IsMatch(strvalue))
+            {
+                var height = GetHeight(strvalue, regexCm);
+                return 150 <= height && height <= 193;
+            }
+            else if(regexIn.IsMatch(strvalue))
+            {
+                var height = GetHeight(strvalue, regexIn);
+                return 59 <= height && height <= 76;
+            }
+
+            return false;
+        }
+
+        private static int GetHeight(string strvalue, Regex regexCm)
+        {
+            var matches = regexCm.Match(strvalue);
+            return int.Parse(matches.Groups[1].Value);
+        }
+    }
+
+    class EyeColorValidation: ValidationAttribute
+    {
+        private IEnumerable<string> list = new List<string> {"amb", "blu", "brn", "gry", "grn", "hzl", "oth"};
+
+        public override bool IsValid(object value)
+        {
+            return list.Contains((string)value);
+        }
     }
 }
--- 2020/AdventOfCodeTest/Tests.cs
+++ 2020/AdventOfCodeTest/Tests.cs
@@ -73,5 +73,55 @@ namespace AdventOfCodeTest
 
             Assert.True(2 == valids, $"Expected: 2. Actual: {valids}");
         }
+
+        [Fact]
+        public void Test4_Invalid()
+        {
+            
+            var lines = new string[]
+            {
+                "eyr:1972 cid:100",
+                "hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926",
+                Environment.NewLine,
+                "iyr:2019",
+                "hcl:#602927 eyr:1967 hgt:170cm",
+                "ecl:grn pid:012533040 byr:1946",
+                Environment.NewLine,
+                "hcl:dab227 iyr:2012",
+                "ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277",
+                Environment.NewLine,
+                "hgt:59cm ecl:zzz",
+                "eyr:2038 hcl:74454a iyr:2023",
+                "pid:3556412378 byr:2007",
+            };
+
+            var valids = PasswordValidator.GetNumberOfValids(lines);
+
+            Assert.True(0 == valids, $"Expected: 0. Actual: {valids}");
+        }
+
+        [Fact]
+        public void Test4_Valid()
+        {
+            var lines = new string[]
+            {
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980",
+                "hcl:#623a2f",
+                Environment.NewLine,
+                "eyr:2029 ecl:blu cid:129 byr:1989",
+                "iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm",
+                Environment.NewLine,
+                "hcl:#888785",
+                "hgt:164cm byr:2001 iyr:2015 cid:88",
+                "pid:545766238 ecl:hzl",
+                "eyr:2022",
+                Environment.NewLine,
+                "iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719",
+            };
+
+            var valids = PasswordValidator.GetNumberOfValids(lines);
+
+            Assert.True(4 == valids, $"Expected: 4. Actual: {valids}");
+        }
     }
 }
